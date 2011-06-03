bda8c934028c015df17d06b302deafe8019a3eea
Bruce <bruce@ecomsmith.com>
2011-06-03 10:10:43 -0700

    initial import, split from bm.gallery

---
+++ signedauth/admin.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.contrib import admin
+from signedauth.models import UserKey, WhitelistedIP, WhitelistedDomain
+
+class UserKey_Inline(admin.StackedInline):
+    model = UserKey
+    max_num = 1
+    readonly_fields = ('key','timestamp')
+
+class WhitelistedIPAdmin(admin.ModelAdmin):
+    list_fields = ('label','ip')
+
+class WhitelistedDomainAdmin(admin.ModelAdmin):
+    list_fields = ('label','ip')
+
+
+admin.site.unregister(User)
+
+UserAdmin.inlines = (UserKey_Inline,)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(WhitelistedIP, WhitelistedIPAdmin)
+admin.site.register(WhitelistedDomain, WhitelistedDomainAdmin)

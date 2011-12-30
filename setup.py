aaefdcab96b419aa3029c84ad595b71223e191d2
Bruce <bruce@ecomsmith.com>
2011-12-30 11:26:03 -0800

    Adding a setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Signedauth',
+      version='1.1',
+      description='Simple signing of urls for use by APIs',
+      author='Bruce Kroeze',
+      author_email='bruce@ecomsmith.com',
+      url='https://github.com/bkroeze/django-signedauth',
+      packages=['signedauth',],
+      install_requires=[
+        'django-keyedcache >= 1.4',
+        ]
+     )
